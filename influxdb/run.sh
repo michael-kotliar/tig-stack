e3fac93845483b2572501d2e7ad7a93d439937d4
Michael Kotliar <misha.kotliar@gmail.com>
2020-07-14 10:43:29 -0400

    Fixed docker image versions

--- influxdb/run.sh
+++ influxdb/run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-set -m
-CONFIG_TEMPLATE="/influxdb.template.conf"
-CONFIG_FILE="/etc/influxdb/influxdb.conf"
-CURR_TIMESTAMP=`date +%s`
-
-mv -v $CONFIG_FILE $CONFIG_FILE.$CURR_TIMESTAMP
-cp -v $CONFIG_TEMPLATE $CONFIG_FILE
-
-exec influxd -config=$CONFIG_FILE 1>>/var/log/influxdb/influxdb.log 2>&1 &
-sleep 5
+influxd -config=/etc/influxdb/influxdb.conf & sleep 5
 
 USER_EXISTS=`influx -host=localhost -port=8086 -execute="SHOW USERS" | awk '{print $1}' | grep grafana | wc -l`
 
@@ -20,4 +11,4 @@ then
   influx -host=localhost -port=8086 -username=grafana -password=grafana -execute="grant all PRIVILEGES on metrics to grafana"
 fi 
 
-tail -f /var/log/influxdb/influxdb.log
+while true; do sleep 1; done
--- telegraf/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -m
-CONFIG_FILE="/telegraf.template.conf"
-hddtemp -d --listen localhost --port 7634 /dev/sd*
-mount --bind /hostfs/proc/ /proc/
-echo "=> Starting Telegraf ..."
-exec telegraf -config $CONFIG_FILE
